453b16af88401bb3d56fe43d7e0bd3227ff76440
Rashed Abdel-Tawab <rashed@linux.com>
2015-07-11 00:47:13 -0400

    himaul: Update fingerprint from 5.1 dev edition

--- cm.mk
+++ cm.mk
@@ -11,5 +11,5 @@ PRODUCT_NAME := cm_himaul
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himaulatt" \
     PRODUCT_NAME="himaulatt_na_gen_unlock" \
-    BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:5.0.2/LRX22G/506785.30:user/release-keys" \
-    PRIVATE_BUILD_DESC="1.32.617.30 CL506785 release-keys"
+    BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:5.1/LMY47O/559710.4:user/release-keys" \
+    PRIVATE_BUILD_DESC="2.8.617.4 CL559710 release-keys"
